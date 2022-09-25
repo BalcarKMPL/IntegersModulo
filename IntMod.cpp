a2c16c68e214cf255e4d24f1a8f98a24b601d57a
BalcarKMPL <mazur.jakub05@gmail.com>
2022-09-25 12:17:54 +0200

    initial commit

---
+++ IntMod.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mazur on 25.09.22.
+//
+
+#include "IntMod.h"
---
+++ IntMod.h
@@ -0,0 +1,17 @@
+//
+// Created by mazur on 25.09.22.
+//
+
+#ifndef INTEGERSMODULO_INTMOD_H
+#define INTEGERSMODULO_INTMOD_H
+
+
+class IntMod {
+private:
+    int val;
+public:
+    IntMod(int a);
+};
+
+
+#endif //INTEGERSMODULO_INTMOD_H
